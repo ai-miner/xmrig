192c7f0e830dd6652f97cc7d66f68d5df85def68
sd <leowest1921@gmail.com>
2024-06-04 14:24:30 +0800

    add

--- embuild.sh
+++ embuild.sh
@@ -14,4 +14,4 @@ emcmake cmake . -DUV_INCLUDE_DIR=./xmrig-deps/gcc/x64/include \
     -DHWLOC_INCLUDE_DIR=./xmrig-deps/gcc/x64/include/ \
     -DHWLOC_LIBRARY=./xmrig-deps/gcc/x64/lib \
     -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF \
-    -DWITH_ARGON2=OFF -DWITH_TLS=OFF -DWITH_MSR=ON
+    -DWITH_ARGON2=OFF -DWITH_TLS=OFF -DWITH_MSR=ON 
