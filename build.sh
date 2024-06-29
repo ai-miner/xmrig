e2be7605da3a450d855f579f0394ce25cf2670e7
sd <leowest1921@gmail.com>
2024-06-29 12:12:37 +0800

    add

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
  apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev libargon2-dev
+ ldconfig
  mkdir build
  cd build
- cmake -DWITH_VAES=OFF  ..
+ cmake -DWITH_VAES=OFF -DWITH_AVX2=OFF ..
  make
