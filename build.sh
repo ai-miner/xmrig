f624516ddafe53bed5e13379f881c171d2a0b56c
sd <leowest1921@gmail.com>
2024-06-29 13:09:58 +0800

    add

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
  #ldconfig
  mkdir build
  cd build
- cmake -DWITH_VAES=OFF -DWITH_AVX2=OFF ..
+ #cmake -DWITH_VAES=OFF -DWITH_AVX2=OFF -DARGON2_LIBRARY=/usr/lib/x86_64-linux-gnu/libargon2.so -DARGON2_INCLUDE_DIR=/usr/include ..
+cmake -DWITH_VAES=OFF \
+    -DARGON2_LIBRARY=/usr/lib/x86_64-linux-gnu/libargon2.so \
+    ..
+
  make
